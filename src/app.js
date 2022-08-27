2b31ef44fffa55f326f997bfadd3c2d23b1b72fe
bceliktug <bulutcan.celiktug@gmail.com>
2022-08-27 15:30:49 +0300

    Js-Bootcamp-Day1

--- src/app.js
+++ src/app.js
@@ -1,36 +1,15 @@
-console.log("Merhaba Kodlama.io")
+console.log("selam baby")
 
-//JS type safe değildir
-let dolarBugun = 9.30
-
-let dolarDun = 9.20
-dolarDun = "9.20"
-{
-    let dolarDun = 9.10
-}
+let dolarDun = 19.5;
+let dolarBugun = 21.8;
 
 console.log(dolarDun)
 
-const euroDun = 11.2
-//euroDun = 11
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
 
-console.log(euroDun)
-
-//array
-//camelCasing
-//PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
-console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
-    console.log("<li>"+konutKredileri[i]+"</li>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+ konutKredileri[i] + "</li>")
 }
-console.log("</ul>")
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
-console.log(konutKredileri)
+// console.log(konutKredileri);
